5d480f5d284d4950818bd9f8e67c456b05696f02
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2025-04-15 11:29:02 +0100

    NavBar logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,11 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
+
+import logo from "../assets/logo.webp";
 
 export default function NavBar() {
-  return <HStack> </HStack>;
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+    </HStack>
+  );
 }
