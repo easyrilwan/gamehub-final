6dcf6ed357853adc47b6b260e9f916653618c3e4
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2025-04-22 22:08:13 +0100

    refactor gamegrid error

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -15,10 +15,10 @@ export default function GameGrid({ gameQuery }: Props) {
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (error) return error && <Text>{error}</Text>;
+
   return (
     <>
-      {error && <Text>{error}</Text>}
-
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
