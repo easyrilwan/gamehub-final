17b89998adefac0bb0ec5d1163ad9b1dd3c4f935
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2025-05-02 12:30:50 +0100

    implement infinite queries

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,6 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Fragment } from "react/jsx-runtime";
+
+import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
 
 import useGames from "../hooks/useGames";
 import { GameQuery } from "../App";
@@ -11,19 +13,22 @@ interface Props {
 }
 
 export default function GameGrid({ gameQuery }: Props) {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useGames(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   if (error) return error && <Text>{error.message}</Text>;
 
   return (
-    <>
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
+    <Box padding="10px">
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
@@ -31,12 +36,22 @@ export default function GameGrid({ gameQuery }: Props) {
             </GameCardContainer>
           ))}
 
-        {data?.results.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
+        {data?.pages.map((page, index) => (
+          <Fragment key={index}>
+            {page.results.map((game) => (
+              <GameCardContainer key={game.id}>
+                <GameCard game={game} />
+              </GameCardContainer>
+            ))}
+          </Fragment>
         ))}
       </SimpleGrid>
-    </>
+
+      {hasNextPage && (
+        <Button onClick={() => fetchNextPage()} marginY={5}>
+          {isFetchingNextPage ? "Loading..." : "Load More"}
+        </Button>
+      )}
+    </Box>
   );
 }
